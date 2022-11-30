ca23ba77a1703868132256cbc557673daf8068c1
saifullahshaikh629 <115484240+saifullahshaikh629@users.noreply.github.com>
2022-11-30 20:33:38 +0500

    abc

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var display = document.getElementById("display");
+
+function getval(a) {
+    display.value +=  a;
+}
+function calculate() {
+    var a = eval(display.value)
+    display.value = a;
+}
+function clear() {
+  display.value0 = clear();    
+}
